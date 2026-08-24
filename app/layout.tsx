7657983827911664b828406c6a0921eca32fc16c
karlosma0416 <1373086696@qq.com>
2026-08-24 13:00:27 +0800

    delete the consultation and upload the new favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,11 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Active Learning | HKU Engineering",
   description: "Active Learning website, Faculty of Engineering, The University of Hong Kong",
+  icons: {
+    icon: "/icon.png",
+    shortcut: "/icon.png",
+    apple: "/icon.png",
+  },
 };
 
 export default function RootLayout({
